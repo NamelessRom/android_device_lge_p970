d28a84f6ffa9b7bd85fc71807ee1576ef20648ec
Evisceration <eviscerationls@gmail.com>
2014-05-28 15:19:48 +0200

    wifi: switch to bcmdhd driver
    Change-Id: I35ed043a5a17556a617a556702ff440a02b03283

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,18 +80,19 @@ endif
 
 BOARD_MOBILEDATA_INTERFACE_NAME := "vsnet0"
 
-BOARD_WLAN_DEVICE                := bcm4329
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4329_ap.bin"
-WIFI_DRIVER_MODULE_NAME          := "wireless"
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wireless.ko"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/system/etc/wifi/nvram.txt config_path=/data/misc/wifi/config"
+BOARD_WLAN_DEVICE                := bcmdhd
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wext
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcmdhd.ko"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/firmware/fw_bcmdhd.bin nvram_path=/system/etc/wifi/nvram.txt config_path=/data/misc/wifi/config"
 WIFI_DRIVER_HAS_LGE_SOFTAP       := true
-BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
-TARGET_HAS_LEGACY_WLAN           := true
 
 BOARD_EGL_CFG       := device/lge/p970/configs/egl.cfg
 ENABLE_WEBGL        := true
