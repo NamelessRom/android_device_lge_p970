cf07b106bcf949823a5fa84a6be9f34b501ac6fd
rac00n <w4shb33r@gmail.com>
2014-06-26 16:05:13 +0200

    liblights: bring back blue led on softkey touch functionality
    setting BUTTON_SYNC to 1 forces all softkey leds to light up the same way,
    so we set it to 0 when the set_light_buttons function is called and we get
    the "blue leds on touch" functionality back, without a huge overhaul of
    liblights and the leds kernel module.
    
    Change-Id: I492b1de82b13b6b09969d0aee8c71adf4e20e3f4

--- liblights/lights.c
+++ liblights/lights.c
@@ -146,7 +146,7 @@ set_light_buttons(struct light_device_t* dev,
 
     pthread_mutex_lock(&g_lock);
     err = write_int(BUTTON_STATE, value ? 1 : 0);
-    write_int(BUTTON_SYNC, 1);
+    write_int(BUTTON_SYNC, 0);
     write_int(BUTTON_BRIGHTNESS, (int)value);
     pthread_mutex_unlock(&g_lock);
     return err;
