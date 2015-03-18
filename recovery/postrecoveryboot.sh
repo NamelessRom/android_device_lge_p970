9384453ebc4713b6bd808713142a03672730f692
Alexander Martinz <eviscerationls@gmail.com>
2015-03-18 16:45:21 +0100

    recovery: add twrp setup
    Change-Id: Ifccf4ba2aaddfd0ae5c5377b81e974fbfea3f13a
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- recovery/postrecoveryboot.sh
+++
@@ -1,8 +0,0 @@
-#!/sbin/sh
-
-sleep 3
-
-## Reset recovery flags
-cat /dev/zero > /dev/block/mmcblk0p12
-
-echo 0 > /sys/devices/platform/omap/omap_i2c.2/i2c-2/2-001a/blink_enable
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -32,3 +32,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.black.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# Recovery
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := twrp.fstab
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/twrp.fstab
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
+include $(BUILD_PREBUILT)
